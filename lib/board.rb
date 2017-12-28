dff1738c10f12153a736ba8470013d8ef415703e
Isil Donmez <isil.donmez@gmail.com>
2017-12-28 20:39:24 +0000

    Finish empty_board visualisation

--- lib/board.rb
+++ lib/board.rb
@@ -18,15 +18,24 @@ class Board
   end
 
   def visualise
-    horizontal_coordinates = "  " + ["a", "b", "c", "d", "e", "f", "g", "h"].join("   ")
-    top_line     = ("┏━━━" * 8 + "┓")
+    horizontal_coordinates = "    " + ["a", "b", "c", "d", "e", "f", "g", "h"].join("   ")
+    top_line     = ("  " + "┏━━━" * 8 + "┓")
     middle1   = ("┃   " * 8 + "┃")
     middle2   = ("┣━━━" * 8 + "┫")
-    bottom_line  = ("┗━━━" * 8 + "┛")
-    middle_line  = [middle1, middle2].join"\n"
+    bottom_line  = ("  " + "┗━━━" * 8 + "┛")
+    middle_lines  = []
+    (8).downto(1) do |num|
+      if num == 1
+        middle_lines << ("#{num} " + middle1 + " #{num}")
+        break
+      else
+        middle_lines << ("#{num} " + middle1 + " #{num}\n")
+        middle_lines << ("  " + middle2 + "  \n")
+      end
+    end
 
-    empty_board = [horizontal_coordinates, top_line, ([middle_line] * 7).join("\n"),
-                    middle1, bottom_line, horizontal_coordinates].join("\n")
+    empty_board = [horizontal_coordinates, top_line, middle_lines.join,
+                    bottom_line, horizontal_coordinates].join("\n")
   end
 
   def update
@@ -35,3 +44,5 @@ end
 
 game = Board.new
 puts game.visualise
+
+
