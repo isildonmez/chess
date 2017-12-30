a921f9d170c126e80dcb00f64c9fe5bac2e6f6ee
Isil Donmez <isil.donmez@gmail.com>
2017-12-30 14:22:38 +0000

    Add check_new_coord method and its test

--- lib/game.rb
+++ lib/game.rb
@@ -17,11 +17,18 @@ class Game
   end
 
   def check_cur_coord(cur_coord)
-    return false if cur_coord.length != 2
-    return false unless ["a", "b", "c", "d", "e", "f", "g", "h"].include?(cur_coord[0])
-    return false unless ["1", "2", "3", "4", "5", "6", "7", "8"].include?(cur_coord[1])
-    return nil unless @board.coord_occupied?(cur_coord)
-    return true
+    return :invalid if cur_coord.length != 2
+    return :invalid unless ["a", "b", "c", "d", "e", "f", "g", "h"].include?(cur_coord[0])
+    return :invalid unless ["1", "2", "3", "4", "5", "6", "7", "8"].include?(cur_coord[1])
+    return :occupied unless @board.coord_occupied?(cur_coord)
+    return :valid
+  end
+
+  def check_new_coord(new_coord)
+    return :invalid if new_coord.length != 2
+    return :invalid unless ["a", "b", "c", "d", "e", "f", "g", "h"].include?(new_coord[0])
+    return :invalid unless ["1", "2", "3", "4", "5", "6", "7", "8"].include?(new_coord[1])
+    return :valid
   end
 
 end
@@ -33,14 +40,16 @@ if __FILE__ == $0
 
   puts "Please enter a coordinate of a piece you want to move. (e.g. a4)"
   cur_coord = gets.chomp.to_sym
-  until chess.check_cur_coord(cur_coord)
-    puts "Please enter a coordinate that is not empty" if check_cur_coord(cur_coord).nil?
-    puts "Please enter a valid coordinate. (e.g. a4, d5)" unless check_cur_coord(cur_coord)
+  until chess.check_cur_coord(cur_coord) == :valid
+    puts "Please enter a coordinate that is not empty" if check_cur_coord(cur_coord) == :occupied
+    puts "Please enter a valid coordinate. (e.g. a4, d5)" unless check_cur_coord(cur_coord) == :invalid
     cur_coord = gets.chomp.to_sym
   end
   puts "Please enter a new coordinate for this piece. (e.g. b6)"
   new_coord = gets.chomp.to_sym
-  until chess.check_new_coord(new_coord)
+  until chess.check_new_coord(new_coord) == :valid
+    puts "Please enter a valid coordinate"
+    new_coord = gets.chomp.to_sym
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,24 +6,42 @@ describe Game do
 
 
   describe "#check_cur_coord" do
-    it "returns nil" do
-      expect(chess.check_cur_coord(:a4)).to eql(nil)
+    it "returns :occupied" do
+      expect(chess.check_cur_coord(:a4)).to eql(:occupied)
     end
 
-    it "returns false" do
-      expect(chess.check_cur_coord(:k4)).to eql(false)
+    it "returns :invalid" do
+      expect(chess.check_cur_coord(:k4)).to eql(:invalid)
     end
 
-    it "returns false" do
-      expect(chess.check_cur_coord(:d9)).to eql(false)
+    it "returns :invalid" do
+      expect(chess.check_cur_coord(:d9)).to eql(:invalid)
     end
 
-    it "returns false" do
-      expect(chess.check_cur_coord(:d32)).to eql(false)
+    it "returns :invalid" do
+      expect(chess.check_cur_coord(:d32)).to eql(:invalid)
     end
 
-    it "returns true" do
-      expect(chess.check_cur_coord(:a2)).to eql(true)
+    it "returns :valid" do
+      expect(chess.check_cur_coord(:a2)).to eql(:valid)
+    end
+  end
+
+  describe "#check_new_coord" do
+    it "returns :invalid" do
+      expect(chess.check_new_coord(:m5)).to eql(:invalid)
+    end
+
+    it "returns :invalid" do
+      expect(chess.check_new_coord(:h0)).to eql(:invalid)
+    end
+
+    it "returns :invalid" do
+      expect(chess.check_new_coord(:e10)).to eql(:invalid)
+    end
+
+    it "returns :valid" do
+      expect(chess.check_new_coord(:f5)).to eql(:valid)
     end
   end
 
